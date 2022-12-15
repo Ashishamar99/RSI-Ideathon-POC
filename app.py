b4694606fbc7e026e9beff8115dff99ec047a118
Ashish Amar <ashishamar1999@gmail.com>
2022-12-16 00:36:33 +0530

    Added one get method

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+data = [
+    {
+        'question': 'What are the policies of the organization ?',
+        'answer': 'In the box URL'
+    },
+    {
+        'question': 'Where is the holiday calendar stored ?',
+        'answer': 'In the box URL'
+    }
+        ]
+
 @app.route('/')
 def index():
     return "Welcome to /"
+
+@app.route('/getAllData', methods=['GET'])
+def getAllData():
+    return jsonify({"DataFromDB":data})
 
 if __name__ =="__main__":
     app.run(debug=True)
