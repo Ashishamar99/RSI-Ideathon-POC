16d686bb5f6426558e09a893ed9415c9444d7fcb
Ashish Amar <ashish.amar@rakuten.com>
2022-12-17 11:21:07 +0530

    updated fuzzy matching to all questions

--- app.py
+++ app.py
@@ -16,13 +16,16 @@ if 'db.json' in os.listdir('.'):
     with open(fullPath, 'rt') as dbFile:
         data = json.load(dbFile)
 
-def calculateFuzzyRatios(originalRecord, frontEndInput):
-    similarityRatio = fuzz.ratio(originalRecord, frontEndInput)
-    tokenSortRatio = fuzz.token_sort_ratio(originalRecord, frontEndInput)
-    averageRatio = sum([similarityRatio, tokenSortRatio])/2
-    print("Average Ratio::", averageRatio, "Original Question:: ", originalRecord)
-    if(averageRatio >= 85):
-        return True
+def calculateFuzzyRatios(frontEndInput, matchingQuestions):
+    for item in data:
+        originalRecord = item["question"].lower()
+        similarityRatio = fuzz.ratio(originalRecord, frontEndInput)
+        tokenSortRatio = fuzz.token_sort_ratio(originalRecord, frontEndInput)
+        averageRatio = sum([similarityRatio, tokenSortRatio])/2
+        print("Average Ratio::", averageRatio, "Original Question:: ", originalRecord)
+        if(averageRatio >= 80):
+            matchingQuestions[originalRecord] = averageRatio
+    print(matchingQuestions)
 
 @app.route('/')
 def index():
@@ -42,16 +45,30 @@ def getAnswer():
     question = question.strip()
     question = question.lower()
     notExists = True
-    for item in data:
-        if calculateFuzzyRatios(item["question"].lower(), question.lower()):
-            notExists = False
-            response = jsonify({"answer":item["answer"], "link":item["link"]})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
+    matchingQuestions = dict()
+    calculateFuzzyRatios(question, matchingQuestions)
+    queryQuestion = str()
+    if len(matchingQuestions) == 1:
+        notExists = False
+        queryQuestion = list(matchingQuestions.keys())[0]
+    elif len(matchingQuestions) > 1:
+        notExists = False
+        maxMatchedValue = max(matchingQuestions.values())
+        for key in matchingQuestions.keys():
+            if matchingQuestions[key] == maxMatchedValue:
+                queryQuestion = key
+                notExists = False
+    
     if notExists:
         response = jsonify({"answer":"I'm sorry, but it looks like the message you have sent is not in a recognizable language or beyond my knowledge. Can you please provide a question or statement in a language that I can understand so that I can better assist you?", "link":"No Link Found"})
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
+    else:
+        for item in data:
+            if item["question"].lower() == queryQuestion:
+                response = jsonify({"answer":item["answer"], "link":item["link"]})
+                response.headers.add("Access-Control-Allow-Origin", "*")
+                return response
 
 if __name__ =="__main__":
     app.run(host='0.0.0.0', port=4005, debug=True)
